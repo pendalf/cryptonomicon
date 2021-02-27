69d447954c15a7a5b0d5b0738553770cb084ee64
Zander <akma_2003@mail.ru>
2021-02-27 19:44:03 +0300

    #16 Криптономикон-5: Работа со списком - Vue.js: практика Работа с адресной строкаой, добавлена пагинация и фильтрация

--- src/App.vue
+++ src/App.vue
@@ -91,9 +91,28 @@
 
       <template v-if="tickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
+
+        <div>
+          <button
+            v-if="page > 1"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            @click="page = page - 1"
+          >
+            Назад
+          </button>
+          <button
+            v-if="hasNextPage"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            @click="page = page + 1"
+          >
+            Вперед
+          </button>
+          <div>Фильтр: <input v-model="filter" /></div>
+        </div>
+        <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in tickers"
+            v-for="t in filteredTickers()"
             :key="t.name"
             @click="select(t)"
             :class="{
@@ -186,6 +205,9 @@ export default {
       tickers: [],
       sel: null,
       graph: [],
+      page: 1,
+      filter: "",
+      hasNextPage: true,
       onLoad: false,
       warning: false,
       coinList: null,
@@ -194,6 +216,18 @@ export default {
   },
 
   created() {
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
+
+    if (windowData.filter) {
+      this.filter = windowData.filter;
+    }
+
+    if (windowData.page) {
+      this.filter = windowData.page;
+    }
+
     const tickersData = localStorage.getItem("cryptonomicon-list");
     if (tickersData) {
       this.tickers = JSON.parse(tickersData);
@@ -206,15 +240,25 @@ export default {
   },
 
   methods: {
+    filteredTickers() {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6;
+
+      const filteredTickers = this.tickers.filter(ticker =>
+        ticker.name.includes(this.filter)
+      );
+      this.hasNextPage = filteredTickers.length > end;
+
+      return filteredTickers.slice(start, end);
+    },
     subscribeToUpdates(tickerName) {
       setInterval(async () => {
         const f = await fetch(
           `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=809f9bac2e6fd2bb9f5ce52996e4bade7525dbfa06844d0fbc01e3d722decc2d`
         );
         const data = await f.json();
-        console.log(data);
         this.tickers.find(t => t.name === tickerName).price =
-          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+          data.USD > 1 ? data.USD.toFixed(2) : data.USD?.toPrecision(2) || null;
         if (this.sel?.name === tickerName) {
           this.graph.push(data.USD);
         }
@@ -230,6 +274,7 @@ export default {
         this.subscribeToUpdates(currentTicker.name);
 
         this.ticker = "";
+        this.filter = "";
         this.coinsAutoComplete = [];
       } else {
         this.warning = true;
@@ -290,6 +335,27 @@ export default {
     onInput() {
       this.warning = false;
       this.coinsAutoComplete = this.coinsSearch();
+    }
+  },
+  watch: {
+    filter() {
+      this.page = 1;
+
+      // const currentURL = new URL(window.location);
+      // const { protocol, host, pathname } = window.location;
+
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
+    page() {
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
     }
   }
 };
